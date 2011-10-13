41b96ff5d99f1bba0f0b8aa0f2332386e6ae19a8
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-10-13 10:40:06 +0100

    duplicate names

--- function.d/dupes.sh
+++ function.d/dupes.sh
@@ -1,6 +1,12 @@
-function dupes ()
+function dups ()
 {
 	[ $# = 0 ] && set -- .
 	find "$@" -name .git -prune -o \( -type f -print0 \) | xargs -r -0 md5sum | sort | uniq --all-repeated=separate -w 32;
+}
+
+function dupnames ()
+{
+	[ $# = 0 ] && set -- .
+	find "$@" -name .git -prune -o \( -type f -printf "%-65f%p\n" \) | sort | uniq --all-repeated=separate -w 64;
 }
 
