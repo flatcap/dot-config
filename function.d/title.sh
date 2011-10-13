f74e2a80b303eab9960f3aaf16eb6f479bbdff24
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-10-13 11:07:22 +0100

    title

--- function.d/title.sh
+++ function.d/title.sh
@@ -1,30 +1,8 @@
-function title ()
+title()
 {
-	local prompt=""
-	local title=""
-	local prefix="$PROMPT_PREFIX"
+	[ -w /dev/stderr ] || return
 
-	#[ -w /dev/stderr ] || return
-	#[[ $TERM =~ ^(xterm|vt100)$ ]] || return
+	[[ $TERM =~ ^(xterm|vt100)$ ]] || return
 
-	[ -n "$prefix" ] && prefix="[$prefix]"
-
-	case $1 in
-	"")
-		title="${PWD/#$HOME/~}"
-		prompt='echo -ne "\033]2;'${prefix}' ${PWD/#$HOME/~}$(parse_git_branch)\007"'
-		;;
-	"-")
-		;;
-	"done")
-		title="done"
-		;;
-	*)
-		title="$*"
-		;;
-	esac
-	title="$prefix $title"
-
-	echo -ne "\033]2;${title}\007" > /dev/stderr
-	PROMPT_COMMAND="echo -n ; $prompt"
+	echo -ne "\033]0;${@}\007" > /dev/stderr
 }
--- title.sh
+++
@@ -1,8 +0,0 @@
-title()
-{
-	[ -w /dev/stderr ] || return
-
-	[[ $TERM =~ ^(xterm|vt100)$ ]] || return
-
-	echo -ne "\033]0;${@}\007" > /dev/stderr
-}
