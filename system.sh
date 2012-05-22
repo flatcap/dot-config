ae19d14deff4c9c1a8c933334389240e61e240ab
Richard Russon (flatcap) <richard.russon@gmail.com>
2012-05-22 17:21:53 +0100

    tidy up system and encrypt

--- system.sh
+++ system.sh
@@ -27,25 +27,27 @@ tar cf $DIR/www.tar                        var/www
 
 cd $DIR
 
-rpm -qa | sort > $DIR/rpm_list
-rpm -qa --last > $DIR/rpm_last
+rpm -qa | sort > rpm_list
+rpm -qa --last > rpm_last
 
 (cd /mnt/tera/tv;     find . -type f) | sort > tv.txt
 (cd /mnt/tera/music;  find . -type f) | sort > music.txt
 (cd /mnt/images;      find . -type f) | sort > images.txt
 
-mkdir $DIR/fdisk
+mkdir fdisk
 #for disk in sda sdb sdc sdd; do
 for disk in sda; do
-	fdisk -lu /dev/$disk            > $DIR/fdisk/$disk.txt
-	dd if=/dev/$disk bs=512 count=1 > $DIR/fdisk/$disk.bin 2> /dev/null
+	fdisk -lu /dev/$disk            > fdisk/$disk.txt
+	dd if=/dev/$disk bs=512 count=1 > fdisk/$disk.bin 2> /dev/null
 done
-tar cf $DIR/fdisk.tar fdisk
-rm -fr $DIR/fdisk
+tar cf fdisk.tar fdisk
+rm -fr fdisk
 
-find $DIR -name \*.tar -o -name \*.txt | xargs -n1 -P4 -- xz -9
+find . \( -name \*.tar -o -name \*.txt \) -print0 | xargs -0 -n1 -P4 -- xz -9
+find . -type f -print0 | xargs -0 -n1 -P4 -- gpg2 --encrypt --recipient "Rich Russon (backup) <rich@flatcap.org>"
+find . ! -name \*.gpg -delete
 
-chown flatcap:flatcap -R $DIR
-chmod 400 $DIR/*
-chmod 500 $DIR
+chown flatcap:flatcap -R .
+chmod 400 *
+chmod 500 .
 
