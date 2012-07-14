4ef4c2e486b65cb5df133ab6a14b2c1308d14d78
Richard Russon (flatcap) <richard.russon@gmail.com>
2012-07-14 01:22:40 +0100

    cdls

--- function/cd.sh
+++ function/cd.sh
@@ -1,11 +1,22 @@
 function cd ()
 {
+	local LS=""
 	if [ -n "$1" ]; then
-		[ -f "$1" ] && set -- "${1%/*}"
+		if [ -f "$1" ]; then
+			set -- "${1%/*}"
+		else
+			local DIR=${1%/*}
+			local END=${1##*/}
+			if [ -d "$DIR" -a "$END" = "ls" ]; then
+				set -- "$DIR"
+				LS="yes"
+			fi
+		fi
 	else
 		[ -n "$CDDIR" ] && set -- "$CDDIR"
 	fi
 	command cd "$@" > /dev/null 2>&1
+	[ "$LS" = "yes" ] && ls --group-directories-first -lL -T 0 --color=auto -I .\*swp -I lost+found
 }
 
 function cddir ()
