6731ecd72219bb1e365c708a647ee39c721bf28d
Richard Russon (flatcap) <richard.russon@gmail.com>
2013-03-22 10:32:41 +0000

    pushd/popd/first/last/n(ext)

--- function/push_pop.sh
+++ function/push_pop.sh
@@ -1,17 +1,39 @@
 function pushd()
 {
-	command pushd "$@" >& /dev/null
+	builtin pushd "$@" >& /dev/null
 	[ $? = 0 ] || echo "pushd failed: $@" > /dev/stderr
 }
 
 function popd()
 {
-	command popd "$@" >& /dev/null
+	builtin popd "$@" >& /dev/null
 	[ $? = 0 ] || echo "popd failed: $@" > /dev/stderr
 }
 
 function dirs()
 {
-	command dirs | tr ' ' '\n' | sed 1d | nl -w2 -n rn
+	builtin dirs -p -v "$@" #| sed 1d
+}
+
+function first()
+{
+	[ $# = 0 ] && return 0
+	for i in "$@"; do
+		if [ ! -d "$i" ]; then
+			echo "dir doesn't exist: $i"
+			continue
+		fi
+		pushd -n "$i"
+	done
+	PUSHD_OLD_DIR="$(pwd)"
+	pushd -0
+	popd -n +1
+}
+
+function last()
+{
+	builtin dirs -c
+	cd "$PUSHD_OLD_DIR"
+	unset PUSHD_OLD_DIR
 }
 
