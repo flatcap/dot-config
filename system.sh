5bdb776813ffd407760e5a22e885338042654b32
Richard Russon (flatcap) <richard.russon@gmail.com>
2013-04-28 18:13:57 +0100

    update for laptop

--- system.sh
+++ system.sh
@@ -18,12 +18,12 @@ cd /
 
 mkdir --parents $DIR
 
-mysqldump --all-databases > "mnt/images/mysql/${DATE}_tables.sql"
+mysqldump --all-databases > "mnt/images/mysql/${DATE}_tables.sql" 2> /dev/null
 
 tar --create --file $DIR/cron.tar                       var/spool/cron
 tar --create --file $DIR/etc.tar                        etc
 tar --create --file $DIR/mysql.tar --exclude mysql.sock mnt/images/mysql	#var/lib/mysql
-tar --create --file $DIR/root.tar                       home/root
+tar --create --file $DIR/root.tar                       root
 tar --create --file $DIR/usr_local.tar                  usr/local
 tar --create --file $DIR/var_log.tar                    var/log
 tar --create --file $DIR/www.tar                        var/www
@@ -34,12 +34,12 @@ cd $DIR
 rpm --query --all | sort > rpm_list
 rpm --query --all --last > rpm_last
 
-(cd /mnt/tera/tv;     find . -type f) | sort > tv.txt
-(cd /mnt/tera/music;  find . -type f) | sort > music.txt
-(cd /mnt/images;      find . -type f) | sort > images.txt
+#(cd /mnt/tera/tv;     find . -type f) | sort > tv.txt
+#(cd /mnt/tera/music;  find . -type f) | sort > music.txt
+#(cd /mnt/images;      find . -type f) | sort > images.txt
 
 mkdir fdisk
-for disk in /dev/sd?; do
+for disk in /dev/sda; do
 	name=${disk##*/}
 	fdisk -lu $disk            > fdisk/$name.txt
 	dd if=$disk bs=512 count=1 > fdisk/$name.bin 2> /dev/null
