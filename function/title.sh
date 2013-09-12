76a302e3cbb208d97f1060a5d8c83b28d2a8f904
Richard Russon (flatcap) <richard.russon@gmail.com>
2013-09-12 11:15:56 +0100

    fix default dir for new terminal windows

--- function/title.sh
+++ function/title.sh
@@ -2,7 +2,8 @@ title()
 {
 	[ -w /dev/stderr ] || return
 
-	[[ $TERM =~ ^(xterm|vt100)$ ]] || return
+	[[ $TERM =~ ^(xterm|xterm-256color|vt100)$ ]] || return
 
-	echo -ne "\033]0;${@}\007" > /dev/stderr
+	echo -ne "\e]0;${@}\a" > /dev/stderr
 }
+
