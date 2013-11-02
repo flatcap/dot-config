bb4757155542010a575453a0bffa6f778d778646
Richard Russon (flatcap) <richard.russon@gmail.com>
2013-11-02 00:54:10 +0000

    workspace tweaks

--- bin/mousemove.sh
+++ bin/mousemove.sh
@@ -5,9 +5,15 @@ COORDS="$(xdotool getmouselocation 2> /dev/null)"
 X=$(sed 's/^x:\([0-9]\+\).*/\1/'  <<< "$COORDS")
 Y=$(sed 's/.*y:\([0-9]\+\).*/\1/' <<< "$COORDS")
 
-if [ $X -lt 1366 ]; then
-	xdotool mousemove 1500 50
+#if [ $X -lt 1366 ]; then
+#	xdotool mousemove 1500 50
+#else
+#	xdotool mousemove 1300 50
+#fi
+
+if [ $X -lt 1920 ]; then
+	xdotool mousemove 2054 50
 else
-	xdotool mousemove 1300 50
+	xdotool mousemove 1854 50
 fi
 
