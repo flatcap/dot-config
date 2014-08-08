cd3ed6c601ad1ff3ff226d45c59b8603213c78ad
Richard Russon (flatcap) <richard.russon@gmail.com>
2014-08-08 14:37:03 +0100

    fix dirname = git branch

--- function/cd.sh
+++ function/cd.sh
@@ -9,32 +9,37 @@ function git_branch()
 function cd ()
 {
 	local LS=""
-	if [ -n "$1" ]; then
+	if [ -n "$1" ]; then						# Argument to cd
 		if [ -f "$1" ]; then
-			set -- "${1%/*}"
+			set -- "${1%/*}"				# cd dir/file
 		else
-			local GDIR="$1"
-			[ "$GDIR" = "." ] && GDIR="master"
+			if [ -d "$1" ]; then 				# directory exists
+				set -- "$1"
+			else						# no dir, check for git branch
+				local GDIR="$1"
+				[ "$GDIR" = "." ] && GDIR="master"	# dir=. => master branch
 
-			if git_branch "$GDIR"; then
-				git checkout -q "$GDIR"
-				export IGNOREEOF=999
-				return
-			fi
-			local DIR=${1%/*}
-			local END=${1##*/}
-			if [ "$1" = "." ]; then
-				set -- "$(pwd -P)"
-			fi
-			if [ -d "$DIR" -a "$END" = "ls" ]; then
-				set -- "$DIR"
-				LS="yes"
+				if git_branch "$GDIR"; then		# git branch exists => checkout branch
+					local GDIR="$1"
+					git checkout -q "$GDIR"
+					export IGNOREEOF=999
+					return
+				fi
+				local DIR=${1%/*}
+				local END=${1##*/}
+				if [ "$1" = "." ]; then			# cd . => change to hard directory
+					set -- "$(pwd -P)"
+				fi
+				if [ -d "$DIR" -a "$END" = "ls" ]; then	# cd dirls => cd dir; ls
+					set -- "$DIR"
+					LS="yes"
+				fi
 			fi
 		fi
-	else
+	else								# No argument to cd
 		[ -n "$CDDIR" ] && set -- "$CDDIR"
 	fi
-	command cd "$@" > /dev/null 2>&1
+	builtin cd "$@" > /dev/null 2>&1
 	RES=$?
 	[ "$LS" = "yes" ] && ls --group-directories-first -lL -T 0 --color=auto -I .\*swp -I lost+found
 	return $RES
