0de3ce8551e1a3a16042fb0962fdbd6d0c13e2fa
Richard Russon (flatcap) <richard.russon@gmail.com>
2015-03-03 19:29:01 +0000

    de-dupe

--- bin/old/above.sh
+++
@@ -1,8 +0,0 @@
-above()
-{
-	title tmp$$
-	usleep 100000;
-	wmctrl -r tmp$$ -b remove,below
-	title
-}
-
--- bin/old/below.sh
+++
@@ -1,8 +0,0 @@
-below()
-{
-	title tmp$$
-	usleep 100000;
-	wmctrl -r tmp$$ -b add,below
-	title
-}
-
