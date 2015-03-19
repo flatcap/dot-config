7fcce843f288ab3b702530aa45d497753161522e
Richard Russon (flatcap) <richard.russon@gmail.com>
2015-03-19 16:44:52 +0000

    drop prompt prefix

--- function/title.sh
+++ function/title.sh
@@ -2,14 +2,11 @@ title()
 {
 	local prompt=""
 	local title=""
-	local prefix="$PROMPT_PREFIX"
-
-	[ -n "$prefix" ] && prefix="[$prefix]"
 
 	case $1 in
 	"")
 		title="${PWD/#$HOME/~}"
-		prompt='echo -ne "\033]2;'${prefix}' ${PWD/#$HOME/~}$(parse_git_branch)\007"'
+		prompt='echo -ne "\033]2;${PWD/#$HOME/~}$(parse_git_branch)\007"'
 		;;
 	"-")
 		;;
@@ -20,7 +17,6 @@ title()
 		title="$*"
 		;;
 	esac
-	title="$prefix $title"
 
 	echo -ne "\033]2;${title}\007" > /dev/stderr
 	# PROMPT_COMMAND="echo -n ; $prompt"
