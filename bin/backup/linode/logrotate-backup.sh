4e8dd0997690b6cda1ccc861be27a3fa1d318125
Richard Russon <rich@flatcap.org>
2015-03-26 13:07:07 +0000

    fix logrotate-backup

--- bin/backup/linode/logrotate-backup.sh
+++ bin/backup/linode/logrotate-backup.sh
@@ -4,6 +4,8 @@ FROM="/var/log/ /var/named/data"
 TO="/backup/linode/log"
 RCPT="Rich Russon (backup) <rich@flatcap.org>"
 
+mkdir --parents "$TO"
+
 # Move files from the log dirs into /backup/log (maintaining the directory hierarchy).
 rsync --archive --prune-empty-dirs --remove-source-files --include '*/' --include '*.gz'        --exclude '*' $FROM $TO
 rsync --archive --prune-empty-dirs --remove-source-files --include '*/' --include '*@*.journal' --exclude '*' $FROM $TO
