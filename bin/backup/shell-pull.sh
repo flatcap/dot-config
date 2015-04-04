f9f0ae03056aca4b5139f0c5b7bd57d6c2ddb4c9
Richard Russon <rich@flatcap.org>
2015-04-04 14:52:22 +0100

    shell-pull.sh

---
+++ bin/backup/shell-pull.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PATH="/usr/bin:/usr/sbin:~/.shell/bash/bin/git"
+
+set -o errexit	# set -e
+set -o nounset	# set -u
+
+renice --priority 19 --pid $$ > /dev/null
+ionice --class 3     --pid $$ > /dev/null
+
+umask 0077
+
+cd "$HOME/.shell"
+
+gr pull
+
