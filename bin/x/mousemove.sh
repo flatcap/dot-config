62b85f68e9d2f276a9b41bb8b3f5277f1d3a6dfe
Richard Russon <rich@flatcap.org>
2015-04-20 22:07:32 +0100

    screen layout diagrams

--- bin/x/mousemove.sh
+++ bin/x/mousemove.sh
@@ -5,30 +5,36 @@ COORDS="$(xdotool getmouselocation 2> /dev/null)"
 X=$(sed 's/^x:\([0-9]\+\).*/\1/'  <<< "$COORDS")
 Y=$(sed 's/.*y:\([0-9]\+\).*/\1/' <<< "$COORDS")
 
-#if [ $X -lt 1366 ]; then
-#	xdotool mousemove 1500 50
-#else
-#	xdotool mousemove 1300 50
-#fi
-
-# # Dual monitors
-# if [ $X -lt 1920 ]; then
-# 	xdotool mousemove 2054 50
-# else
-# 	xdotool mousemove 1854 1050
-# fi
-
-# Laptop, split windows
+# Laptop: 1366x768
+# ┌──────┬──────┐
+# │      │      │
+# │     .│.     │
+# └──────┴──────┘
 if [ $X -lt 683 ]; then
 	xdotool mousemove 700 750
 else
 	xdotool mousemove 660 750
 fi
 
-# Monitor, split windows
+# Monitor: 1920x1080
+# ┌──────┬──────┐
+# │      │      │
+# │     .│.     │
+# └──────┴──────┘
 # if [ $X -lt 960 ]; then
 # 	xdotool mousemove 977 1064
 # else
 # 	xdotool mousemove 943 1064
+# fi
+
+# Monitor/Laptop: 3286x1080
+# ┌─────────────┐
+# │             │┌───────┐
+# │            .││.      │
+# └─────────────┘└───────┘
+# if [ $X -lt 1920 ]; then
+# 	xdotool mousemove 2054 50
+# else
+# 	xdotool mousemove 1854 1050
 # fi
 
