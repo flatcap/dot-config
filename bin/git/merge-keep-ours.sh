e8bf3e2f87659bf7ebac7b3ed331924dc89b33e4
Richard Russon <rich@flatcap.org>
2015-04-22 23:42:47 +0100

    lots of git filters

---
+++ bin/git/merge-keep-ours.sh
@@ -0,0 +1,5 @@
+# I want to keep MY version when there is a conflict
+# Nothing to do: %A (the second parameter) already contains my version
+# Just indicate the merge has been successfully "resolved" with the exit status
+touch "$2"
+exit 0
---
+++ bin/git/merge-keep-theirs.sh
@@ -0,0 +1,4 @@
+# I want to keep THEIR version when there is a conflict
+# Copy their version over ours and report success
+cp -f "$3" "$2"
+exit 0
