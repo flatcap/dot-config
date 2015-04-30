d976564b0ab03b87bf5790cb8e54a64abc69f485
Richard Russon <rich@flatcap.org>
2015-04-30 13:10:56 +0100

    fix git-backup

--- bin/backup/linode/git-backup.sh
+++ bin/backup/linode/git-backup.sh
@@ -19,7 +19,7 @@ TAR_OPTS="--warning=no-file-changed"
 mkdir --parents $DIR
 
 # Run fsck and garbage collection on the git repos
-su - gitolite3 -c "bin/gr tidy >& /dev/null; reset-dates; drop-cache"
+su - gitolite3 -c "gr tidy >& /dev/null; reset-dates; drop-cache"
 
 cd /var/lib
 tar $TAR_OPTS --create --file "$GIT_TAR" gitolite3
