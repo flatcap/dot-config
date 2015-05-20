30cff1432eb31f39b2b55f1b4f2159e73325b117
Richard Russon <rich@flatcap.org>
2015-05-20 13:56:49 +0100

    unused

--- function/dupes.sh
+++ function/dupes.sh
@@ -1,9 +1,3 @@
-function dups ()
-{
-	[ $# = 0 ] && set -- .
-	find "$@" -name .git -prune -o \( -type f -print0 \) | xargs -r -0 md5sum | sort | uniq --all-repeated=separate -w 32;
-}
-
 function dupnames ()
 {
 	[ $# = 0 ] && set -- .
