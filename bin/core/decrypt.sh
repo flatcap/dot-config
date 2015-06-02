3da56b6f277dcc1f0bd6652d80d241df3d982ca9
Richard Russon <rich@flatcap.org>
2015-06-02 18:08:14 +0100

    lower priority of en/decrypt

--- bin/core/decrypt.sh
+++ bin/core/decrypt.sh
@@ -2,6 +2,14 @@
 
 [ -n "$1" ] || exit 1
 
+set -o errexit	# set -e
+set -o nounset	# set -u
+
+renice --priority 19 --pid $$ > /dev/null
+ionice --class 3     --pid $$ > /dev/null
+
+umask 0077
+
 for i in "$@"; do
 	echo $i
 	gpg2 -d "$i" > "${i%.gpg}"
--- bin/core/encrypt.sh
+++ bin/core/encrypt.sh
@@ -4,6 +4,14 @@
 
 RCPT="Rich Russon (backup) <rich@flatcap.org>"
 
+set -o errexit	# set -e
+set -o nounset	# set -u
+
+renice --priority 19 --pid $$ > /dev/null
+ionice --class 3     --pid $$ > /dev/null
+
+umask 0077
+
 for i in "$@"; do
 	echo $i
 	gpg2 -e -R "$RCPT" "$i"
