55a5afb18ea389e6a33c8d0a944af8c86f6ad411
Richard Russon <rich@flatcap.org>
2015-06-07 16:37:20 +0100

    limit to interactive shells

--- bin/x/set-cols.sh
+++ bin/x/set-cols.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+[[ $- != *i* ]] && exit 0
 echo -en "\e[18t" # returns \e[8;??;??t
 IFS='[;'
 read -d t -s esc params
@@ -6,4 +7,5 @@ set -- $params
 [ $# = 3 -a "$1" = 8 ] && shift
 [ $# != 2 ] && echo error >&2 && exit 1
 # echo setting terminal to "$2x$1" >&2
+stty sane
 stty rows "$1" cols "$2"
