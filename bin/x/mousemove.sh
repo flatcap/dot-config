171ce1142181f89f4c80b3eb6b3310bfa8f58a89
Richard Russon <rich@flatcap.org>
2015-06-22 03:12:47 +0100

    tweak mouse positions

--- bin/x/mousemove.sh
+++ bin/x/mousemove.sh
@@ -10,22 +10,22 @@ Y=$(sed 's/.*y:\([0-9]\+\).*/\1/' <<< "$COORDS")
 # │      │      │
 # │     .│.     │
 # └──────┴──────┘
-# if [ $X -lt 683 ]; then
-# 	xdotool mousemove 700 750
-# else
-# 	xdotool mousemove 660 750
-# fi
+if [ $X -lt 683 ]; then
+	xdotool mousemove 700 750
+else
+	xdotool mousemove 660 750
+fi
 
 # Monitor: 1920x1080
 # ┌──────┬──────┐
 # │      │      │
 # │     .│.     │
 # └──────┴──────┘
-if [ $X -lt 960 ]; then
-	xdotool mousemove 977 1064
-else
-	xdotool mousemove 943 1064
-fi
+# if [ $X -lt 960 ]; then
+# 	xdotool mousemove 977 1064
+# else
+# 	xdotool mousemove 943 1064
+# fi
 
 # Monitor/Laptop: 3286x1080
 # ┌─────────────┐
@@ -33,7 +33,7 @@ fi
 # │            .││.      │
 # └─────────────┘└───────┘
 # if [ $X -lt 1920 ]; then
-# 	xdotool mousemove 2054 50
+# 	xdotool mousemove 1940 880
 # else
 # 	xdotool mousemove 1854 1050
 # fi
