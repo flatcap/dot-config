1563c61afc88170d5dc1b6c3b1b5ca133ab7f19e
Richard Russon <rich@flatcap.org>
2015-07-13 13:29:45 +0100

    update existing dates

--- bin/backup/date-it.sh
+++ bin/backup/date-it.sh
@@ -5,7 +5,11 @@
 DATE=$(date '+%F')
 
 for FILE in "$@"; do
-	NEW="${DATE}_$FILE"
+	if [[ "$FILE" =~ ^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]_ ]]; then
+		NEW=${DATE}_${FILE:11}
+	else
+		NEW="${DATE}_$FILE"
+	fi
 	mv "$FILE" "$NEW"
 	echo "$NEW"
 done
