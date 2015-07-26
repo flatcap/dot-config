a04359e92508aca068f151b6a587fac971fa32e3
Richard Russon <rich@flatcap.org>
2015-07-26 19:29:36 +0100

    copy, then push

--- bin/backup/google/push-to-google.sh
+++ bin/backup/google/push-to-google.sh
@@ -6,13 +6,14 @@ set -o nounset	# set -u
 renice --priority 19 --pid $$ > /dev/null
 ionice --class 3     --pid $$ > /dev/null
 
+GDIR="/mnt/space/google"
+if [ -d "$GDIR" ]; then
+	echo "Copying files to $GDIR"
+	rsync -av . "$GDIR"/
+fi
+
 echo "Pushing files to google:"
 echo
 
 rclone --stats=10s --transfers=1 copy . gdrive:
-
-GDIR="/mnt/space/google"
-[ -d "$GDIR" ] || exit
-
-rsync -av . "$GDIR"/
 
