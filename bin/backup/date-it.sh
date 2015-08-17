c4df780bcf260fd8d25a9e1cb47361a5dfa034d6
Richard Russon <rich@flatcap.org>
2015-08-17 15:02:57 +0100

    correctly work with paths

--- bin/backup/date-it.sh
+++ bin/backup/date-it.sh
@@ -4,13 +4,20 @@
 
 DATE=$(date '+%F')
 
-for FILE in "$@"; do
+for ARG in "$@"; do
+	if [[ $ARG =~ / ]]; then
+		FILE="${ARG##*/}"
+		DIR="${ARG%/*}/"
+	else
+		FILE="$ARG"
+		DIR=""
+	fi
 	if [[ "$FILE" =~ ^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]_ ]]; then
 		NEW=${DATE}_${FILE:11}
 	else
 		NEW="${DATE}_$FILE"
 	fi
-	mv "$FILE" "$NEW"
+	mv "$DIR$FILE" "$DIR$NEW"
 	echo "$NEW"
 done
 
