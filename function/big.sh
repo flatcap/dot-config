d65ffb7346edf093f85c4691547c07befc96132f
Richard Russon <rich@flatcap.org>
2015-08-24 15:38:03 +0100

    ignore mounts, links, devices, etc

--- function/big.sh
+++ function/big.sh
@@ -1,9 +1,18 @@
-big()
+function big()
 {
 	shopt -s nullglob
 
 	local LINES=$(($(tput lines) - 3))
-
-	du --summarize --block-size=1K --one-file-system * .??* | sort --numeric-sort --reverse | head --lines $LINES | column --table
+	local LIST=(* .??*)
+	local WORK=()
+	local HERE=$(pwd -P)
+	HERE="${HERE%/}"
+	for i in "${LIST[@]}"; do
+		[ -L "$i" ] && continue
+		[ -f "$i" -o -d "$i" ] || continue
+		[ "${i##*/}" = "lost+found" ] && continue
+		grep -q " $HERE/$i " /proc/mounts || WORK+=($i)
+	done
+	du --summarize --block-size=1K --one-file-system "${WORK[@]}" | sort --numeric-sort --reverse | head --lines $LINES
 }
 
