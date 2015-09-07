67646e8b30dbecc887efe34425a217957eb7cf67
Richard Russon <rich@flatcap.org>
2015-09-07 14:53:06 +0100

    whitespace

--- function/big.sh
+++ function/big.sh
@@ -11,7 +11,7 @@ function big()
 		[ -L "$i" ] && continue
 		[ -f "$i" -o -d "$i" ] || continue
 		[ "${i##*/}" = "lost+found" ] && continue
-		grep -q " $HERE/$i " /proc/mounts || WORK+=($i)
+		grep -q " $HERE/$i " /proc/mounts || WORK+=("$i")
 	done
 	du --summarize --block-size=1K --one-file-system "${WORK[@]}" | sort --numeric-sort --reverse | head --lines $LINES
 }
