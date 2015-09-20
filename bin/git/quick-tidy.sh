4df216d9ba420b8646ac3ebdb3b2e7c7fe6cfc31
Richard Russon <rich@flatcap.org>
2015-09-20 14:50:09 +0100

    cd first

--- bin/git/quick-tidy.sh
+++ bin/git/quick-tidy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -o errexit	# set -e
+set -o nounset	# set -u
+
+cd ~/repositories
+
 for i in $(find . -type d -name \*.git); do
 	COUNT=$(find $i -type f | wc -l)
 	[ $COUNT -lt 12 ] && continue
