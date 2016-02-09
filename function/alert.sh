f6a0fa78bf74cb58b930a65eee9055a578aade80
Richard Russon <rich@flatcap.org>
2016-02-09 15:14:06 +0000

    louder alert

--- function/alert.sh
+++ function/alert.sh
@@ -2,7 +2,7 @@ function alert()
 {
 	local COUNT;
 	[ $# = 1 ] && COUNT=$1 || COUNT=1
-	(repeat $COUNT play --volume 0.1250 /usr/share/sounds/gnome/default/alerts/glass.ogg &) &> /dev/null
+	(repeat $COUNT play --volume 0.50 /usr/share/sounds/gnome/default/alerts/glass.ogg &) &> /dev/null
 }
 
 export -f alert
