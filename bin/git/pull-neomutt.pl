bc6d68c2c703acfb8b8b707d9f05ee721bda81b1
Richard Russon <rich@flatcap.org>
2016-06-23 01:49:56 +0100

    mirror github

---
+++ bin/git/pull-neomutt.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+our $VERSION = 0.1;
+
+use Data::Dumper;
+use LWP::Simple;
+use JSON qw(decode_json);
+
+my $url = 'https://api.github.com/orgs/neomutt/repos';
+
+chdir ("/var/lib/gitolite3/repositories/github/neomutt") or die "$!";
+
+my $json = get ($url);
+
+my $decoded_json = decode_json ($json);
+
+# print Dumper $decoded_json;
+
+foreach (@{$decoded_json}) {
+	my $repo = $_;
+
+	my $name = $repo->{'name'};
+	my $desc = $repo->{'description'};
+
+	printf "%s %s\n", $name, $desc;
+
+	my $dir = $name . '.git';
+	if (!-d $dir) {
+		my @clone = ('git', 'clone', '--bare', $repo->{'html_url'}, $dir);
+		system(@clone) == 0 || die 'git clone failed';
+
+		my $FH;
+		my $file;
+
+		$file = $dir . '/description';
+		if (!open $FH, '>', $file) {
+			die "Can't create file: '$file'\n";
+		}
+
+		if (!print {$FH} $desc) {
+			printf "Write failed for: %s\n", $file;
+		}
+
+		if (!close $FH) {
+			printf "Close failed for: %s\n", $file;
+		}
+
+		$file = $dir . '/config';
+		my $data = "\tfetch = +refs/heads/*:refs/remotes/origin/*\n[gitweb]\n\towner = Richard Russon\n";
+		if (!open $FH, '>>', $file) {
+			die "Can't create file: '$file'\n";
+		}
+
+		if (!print {$FH} $data) {
+			printf "Write failed for: %s\n", $file;
+		}
+
+		if (!close $FH) {
+			printf "Close failed for: %s\n", $file;
+		}
+	}
+}
+
