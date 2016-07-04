3520de0af0bf4a9227a82e97bc60ee69919044a2
Richard Russon <rich@flatcap.org>
2016-07-04 12:52:04 +0100

    tar, encrypt and split a directory

---
+++ bin/backup/tar-encrypt-split.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -o errexit	# set -e
+set -o nounset	# set -u
+
+renice --priority 19 --pid $$ > /dev/null
+ionice --class 3     --pid $$ > /dev/null
+
+RCPT="Rich Russon (backup) <rich@flatcap.org>"
+DIR="${1%/}"
+SUFFIX=".tar.gpg"
+NUM_LENGTH=2
+CHUNK_SIZE="1G"
+
+[ -d "$DIR" ] || exit 1
+
+tar --sort=name -cvf - "$DIR" \
+	| gpg2 --encrypt --hidden-recipient "$RCPT" --compress-algo none -o - \
+	| split --bytes "$CHUNK_SIZE" --additional-suffix="$SUFFIX" --numeric-suffixes=1 --suffix-length="$NUM_LENGTH" - "${DIR}-"
+
