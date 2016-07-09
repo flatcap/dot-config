d6589c38164d9d8777472c14658bda3ff8e813dc
Richard Russon <rich@flatcap.org>
2016-07-09 17:00:29 +0100

    fix regex

--- function/g.sh
+++ function/g.sh
@@ -1,7 +1,7 @@
 function g()
 {
 	if [ $# = 0 ]; then
-		git status --short 2> /dev/null || git branch --verbose --verbose --color | green 'ahead [0-9]\\+' | red 'behind [0-9]\\+'
+		git status --short 2> /dev/null || git branch --verbose --verbose --color | green 'ahead [0-9]\+' | red 'behind [0-9]\+'
 	else
 		git "$@"
 	fi
