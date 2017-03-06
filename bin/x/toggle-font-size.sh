68b178b8dcd2eaf50f0817802c5d4186bb56cf74
Richard Russon <rich@flatcap.org>
2017-03-06 02:24:20 +0000

    toggle both terms

--- bin/x/toggle-font-size.sh
+++ bin/x/toggle-font-size.sh
@@ -3,13 +3,19 @@
 SIZE_SMALL="'Source Code Pro for Powerline Medium 12'"
 SIZE_LARGE="'Source Code Pro for Powerline Medium 14'"
 
-KEY="/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/font"
+# Gnome terminal
+KEY1="/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/font"
 
-S=$(dconf read "$KEY")
+# Terminix
+KEY2="/com/gexperts/Terminix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/font"
+
+S=$(dconf read "$KEY1")
 
 if [ "$S" = "$SIZE_SMALL" ]; then
-	dconf write $KEY "$SIZE_LARGE"
+	dconf write $KEY1 "$SIZE_LARGE"
+	dconf write $KEY2 "$SIZE_LARGE"
 else
-	dconf write $KEY "$SIZE_SMALL"
+	dconf write $KEY1 "$SIZE_SMALL"
+	dconf write $KEY2 "$SIZE_SMALL"
 fi
 
