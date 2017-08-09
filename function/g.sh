f1b4fa2522d0d0d2cd1ecac6f6ebb59ac9fccafb
Richard Russon <rich@flatcap.org>
2017-08-10 00:12:10 +0100

    wrap status

--- function/g.sh
+++ function/g.sh
@@ -1,7 +1,17 @@
 function g()
 {
+	local STATUS
+	local LINES
 	if [ $# = 0 ]; then
-		git status --short 2> /dev/null || git branch --verbose --verbose --color | green 'ahead [0-9]\+' | red 'behind [0-9]\+' | reverse gone
+		STATUS="$(git status --short 2> /dev/null)"
+		LINES=$(echo "$STATUS" | wc -l)
+		if [ $LINES = 0 ]; then
+			git branch --verbose --verbose --color | green 'ahead [0-9]\+' | red 'behind [0-9]\+' | reverse gone
+		elif [ $LINES -lt 20 ]; then
+			echo "$STATUS" | yellow "\<M\>"
+		else
+			echo "$STATUS" | column -c $(tput cols) | yellow "\<[CRMA]\>  " | red '\<[UD]\+ \+[^ ]\+\>'
+		fi
 	else
 		git "$@"
 	fi
