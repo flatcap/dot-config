10ed876c3feddf54e0287594893101d6f802aac9
Richard Russon <rich@flatcap.org>
2017-11-19 01:36:17 +0000

    improved folding

--- function/g.sh
+++ function/g.sh
@@ -3,16 +3,22 @@ function g()
 	local STATUS
 	local LINES
 	if [ $# = 0 ]; then
-		STATUS="$(git status --short 2> /dev/null)"
-		LINES=$(echo "$STATUS" | wc -l)
-		if [ $LINES = 0 ]; then
+		# STATUS="$(git status --short 2> /dev/null | yellow '^[CRMA ][CRMA ] ' | red '^[AUD][AUD] \+[^ ]\+')"
+		STATUS="$(git status --short 2> /dev/null | sed 's/^/</')"
+		if [ -z "$STATUS" ]; then
 			git branch --verbose --verbose --color | green 'ahead [0-9]\+' | red 'behind [0-9]\+' | reverse gone
-		elif [ $LINES -lt 20 ]; then
-			echo "$STATUS" | yellow "\<M\>"
 		else
-			echo "$STATUS" | column -c $(tput cols) | yellow "\<[CRMA]\>  " | red '\<[UD]\+ \+[^ ]\+\>'
+			LINES=$(echo "$STATUS" | wc -l)
+			(
+			if [ $LINES -lt 30 ]; then
+				echo "$STATUS"
+			else
+				echo "$STATUS" | column -c $(tput cols) | expand -t8
+			fi
+			) | red '<[AUD ][AUD ] [^ ]\+' | yellow '<[CRMA][CRMA ] ' | green '<[ AM][CRMA] ' | magenta '<?? ' | sed 's/<//g'
 		fi
 	else
 		git "$@"
 	fi
 }
+
